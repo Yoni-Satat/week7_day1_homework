ac2bee0176b008dbe807aa6cee995d0a1c5c8cf3
Yoni Satat <yoni.satat@gmail.com>
2017-11-06 16:32:32 +0000

    refactored Open_Closeed, all tests passed

---
+++ Open_Closed/app/src/main/java/example/codeclan/com/openclosed/Food.java
@@ -0,0 +1,8 @@
+package example.codeclan.com.openclosed;
+
+/**
+ * Created by user on 06/11/2017.
+ */
+
+public class Food {
+}
---
+++ Open_Closed/app/src/main/java/example/codeclan/com/openclosed/TopHat.java
@@ -0,0 +1,8 @@
+package example.codeclan.com.openclosed;
+
+/**
+ * Created by user on 06/11/2017.
+ */
+
+class TopHat extends Food {
+}
